9611312eaa4bbedc9ac04c76fda54dee05067e8c
Eric Rosebrock <webfreakeric@gmail.com>
2016-07-26 11:10:33 -0400

    Removed user double check password requirements for register / signup page.

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -158,7 +158,7 @@ module.exports = function(MeanUser) {
             req.assert('email', 'You must enter a valid email address').isEmail();
             req.assert('password', 'Password must be between 8-20 characters long').len(8, 20);
             // req.assert('username', 'Username cannot be more than 20 characters').len(1, 20);
-            req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+            // req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
 
             var errors = req.validationErrors();
             if (errors) {

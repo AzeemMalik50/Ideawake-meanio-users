c343bfb31cb1c7e11a6ab9bb76b02288a7543471
Tim Elfelt <timelf123@gmail.com>
2017-02-22 14:11:29 -0600

    try a fix for aurora localstorage not working, at least dont throw error..

--- public/routes/auth.js
+++ public/routes/auth.js
@@ -3,9 +3,22 @@
 //Setting up route
 angular.module('mean.users').config(['$httpProvider', 'jwtInterceptorProvider',
   function($httpProvider, jwtInterceptorProvider) {    
-        
+
+  	function localStorageTest() {
+	    var test = 'test';
+	    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+	    } catch(e) {
+        return false;
+	    }
+		}
+
     jwtInterceptorProvider.tokenGetter = function() {
-      return localStorage.getItem('JWT');
+      if (lsTest()) {
+      	return localStorage.getItem('JWT');
+      } else { return '' }
     };
 
     $httpProvider.interceptors.push('jwtInterceptor');

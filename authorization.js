088639f5a02dbe7aaba4731b149333bc87bc0624
Tim Elfelt <timelf123@gmail.com>
2018-02-15 17:12:49 -0600

    omit userProfile from JWT

--- authorization.js
+++ authorization.js
@@ -73,7 +73,7 @@ exports.isMongoId = function(req, res, next) {
 exports.generateAuthToken = function(MeanUser) {
   return (req, res, next) => {
     try {
-      let payload = _.omit(req.user._doc, ['salt', 'hashed_password']);    
+      let payload = _.omit(req.user._doc, ['salt', 'hashed_password', 'userProfile']);    
       let escaped, token;
 
       if (MeanUser) {

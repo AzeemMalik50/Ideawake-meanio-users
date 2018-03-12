2153639b4c6bcd47f446290a4272949d525956d4
Tim Elfelt <timelf123@gmail.com>
2018-03-12 14:47:06 -0500

    hotfix(jwt) - bail out on bad JWT, clear rft and JWT and log for trackjs

--- public/routes/auth.js
+++ public/routes/auth.js
@@ -2,48 +2,57 @@
 
 //Setting up route
 angular.module('mean.users').config(['$httpProvider', 'jwtInterceptorProvider',
-  function($httpProvider, jwtInterceptorProvider) {    
+  function ($httpProvider, jwtInterceptorProvider) {
 
-  	function localStorageTest() {
-	    var test = 'test';
-	    try {
+    function localStorageTest() {
+      var test = 'test';
+      try {
         localStorage.setItem(test, test);
         localStorage.removeItem(test);
         return true;
-	    } catch(e) {
+      } catch (e) {
         return false;
-	    }
-		}
+      }
+    }
 
-    jwtInterceptorProvider.tokenGetter = ['$cookies', '$location', '$http', 'jwtHelper', function($cookies, $location, $http, jwtHelper) {
+    jwtInterceptorProvider.tokenGetter = ['$cookies', '$location', '$http', 'jwtHelper', function ($cookies, $location, $http, jwtHelper) {
       if (localStorageTest()) {
         var lcJwt = localStorage.getItem('JWT');
         var rft = localStorage.getItem('rft');
-        if(lcJwt && rft && jwtHelper.isTokenExpired(lcJwt)){
-          var user = jwtHelper.decodeToken(lcJwt);
+        if (lcJwt) {
+          var user;
+          try {
+            user = jwtHelper.decodeToken(lcJwt);
+          } catch (err) {
+            console.log('tokens', lcJwt, rft);
+            console.error(err);
+            localStorage.removeItem('JWT');
+            localStorage.removeItem('rft');
+            return;
+          }
           return $http({
             url: '/api/refreshtoken',
             skipAuthorization: true,
             method: 'POST',
             data: { refreshToken: rft, id: user._id }
-        }).then(function(response) {
-              if(response && response.data){
-                localStorage.setItem('JWT', response.data.token);
-                return response.token;
-              }
-            })
-            .catch(function(err) {
+          }).then(function (response) {
+            if (response && response.data) {
+              localStorage.setItem('JWT', response.data.token);
+              return response.token;
+            }
+          })
+            .catch(function (err) {
               console.log(err);
               localStorage.removeItem('JWT');
               return;
             });
         } else {
-          return lcJwt; 
+          return lcJwt;
         }
       } else {
         $cookies.put('nolocalstorage', 'true');
         $location.url('/unsupported-browser');
-      	// return $cookies.get('id_token');
+        // return $cookies.get('id_token');
       }
     }];
 

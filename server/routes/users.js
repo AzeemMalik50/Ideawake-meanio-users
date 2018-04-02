2b2d8ef3fac1e63af44ca703568b960bd2d89817
Farhan <ftahir@ssidecisions.com>
2018-04-02 11:57:53 +0500

    Change endpoint from GET to POST due to exclude fitler having ids in it and making url too long

--- server/routes/users.js
+++ server/routes/users.js
@@ -271,5 +271,5 @@ module.exports = function (MeanUser, app, circles, database, passport) {
       }), users.authCallback);
   }
 
-  app.get('/api/users/search', users.search);
+  app.post('/api/users/search', users.search);
 };

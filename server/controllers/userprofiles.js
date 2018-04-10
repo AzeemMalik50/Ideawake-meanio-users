4a05520bb072fe306215f2bc159b174611de6d53
Farhan <ftahir@ssidecisions.com>
2018-04-10 19:04:59 +0500

    secondary email added for user model

--- server/controllers/userprofiles.js
+++ server/controllers/userprofiles.js
@@ -17,7 +17,7 @@ module.exports = function(UserProfiles, http) {
         if(req.user && req.user._id) {
 
             UserProfile.findOne({'user' : req.user._id})
-                .populate('user', 'name username email')
+                .populate('user', 'name username email secondaryEmail')
                 .exec(function(err, userProfile) {
                     if (err) {
                         req.log.info(err);
